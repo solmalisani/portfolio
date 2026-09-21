6b6833c5416b28f039cfed81713d2d76e58c8479
Sol Malisani <solmalisani@gmail.com>
2026-09-21 15:44:08 +0200

    Reorganizing files, deleting old ones and adidng text to about page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,25 @@
 import Nav from "../components/Nav"
 import Footer from "../components/Footer"
-import ContactForm from "../components/ContactForm"
-import { Component as PhotoLoopSlider } from "@/components/ui/photo-loop-infinite-slider"
-import { Press } from "@/components/About"
+import ContactForm from "./Contact"
+import { Component as PhotoLoopSlider } from "@/components/photo-loop-infinite-slider"
+import AboutSection from './About'
 
 export default function DemoOne() {
   return (
-    <div className="relative w-full h-screen overflow-hidden">
+    <div className="relative w-full min-h-screen">
       <header className="fixed top-0 left-0 right-0 z-50 pointer-events-auto">
         <Nav />
       </header>
 
+      {/* HOME PAGE */}
       <main id="home" className="flex-1 flex items-center justify-center min-h-screen">
         <PhotoLoopSlider />
       </main>
 
+      {/* ABOUT PAGE */}
+      <AboutSection />
+
+      {/* CONTACT PAGE */}
       <section
         id="contact"
         className="min-h-screen w-full flex flex-col justify-center items-center px-8 pt-24 pb-16 relative z-10"
@@ -26,21 +31,7 @@ export default function DemoOne() {
         <ContactForm />
       </section>
 
-      <section
-        id="about"
-        className="min-h-screen w-full flex flex-col justify-center items-center px-8 pt-24 pb-16 relative z-10"
-        style={{
-          backgroundColor: "var(--background)",
-          borderTop: "1px solid var(--border)"
-        }}
-      >
-      <Press
-        // src="/images/Foto.jpg"
-        // alt="ASCII background"
-        className="absolute inset-0 h-full w-full pointer-events-none  object-cover rounded-2xl"
-      />
-      </section>
-
+      {/* FOOTER */}
       <footer className="fixed bottom-0 left-0 right-0 z-50 pointer-events-auto">
         <Footer />
       </footer>
